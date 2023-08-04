1ad3c4b997f0006a465450f0ecda8e025ece4071
dta <daniel.takacs@sofiadigital.com>
2023-08-04 22:12:00 +0300

    add dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolistreact/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-  }
+  } 
 })
